2625dbf4466b526c09082c35b82adf1012d1611e
Master.G <dearunclexiao@qq.com>
2013-08-05 21:59:52 +0800

    init

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ *  main.c
+ *  sanguosha
+ *
+ *  Created by Master.G on 13-8-5.
+ *  Copyright (c) 2013 Master.G. All rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    /* insert code here... */
+    printf("Hello, World!\n");
+    return 0;
+}
+
