a0aeb943b817e5b6b3a05fe586bf7b21bc93fd68
Master.G <dearunclexiao@qq.com>
2013-08-08 15:19:40 +0800

    cards

---
+++ cards.h
@@ -0,0 +1,22 @@
+/*
+ *  cards.h
+ *  sanguosha
+ *
+ *  Created by Master.G on 13-8-8.
+ *  Copyright (c) 2013 Master.G. All rights reserved.
+ */
+
+#ifndef CARDS_H_
+#define CARDS_H_
+
+/*
+ *   +--------+--------+--------+--------+
+ *   |xxxxxxxx|xxxxxxxx|xxxxxxxx|cdhsrrrr|
+ *   +--------+--------+--------+--------+
+ *
+ *   r = rank of card (deuce=0,trey=1,four=2,five=3,...,ace=12)
+ *   cdhs = suit of card
+ *
+ */
+
+#endif  /* CARDS_H_ */
